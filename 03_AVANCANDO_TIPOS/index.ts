06f056bc6c2575f73940571a30edc0654e096730
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-03-27 22:26:34 +0044

    type alias

--- 03_AVANCANDO_TIPOS/index.ts
+++ 03_AVANCANDO_TIPOS/index.ts
@@ -113,3 +113,13 @@ function showUserRole(role: boolean | string) {
 
 console.log(showUserRole(false));
 console.log(showUserRole("Admin"));
+
+// 12 - type alias
+type ID = string | number;
+
+function showId(id: string | ID) {
+  console.log(`O ID é: ${id}`);
+}
+
+showId(1);
+showId("200");
