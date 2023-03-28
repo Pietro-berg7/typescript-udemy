eb20c5a229529bcf59f705a3ddd59e7df89d4ebd
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-03-28 04:50:47 +0044

    checando se valor existe

--- 04_NARROWING/index.js
+++ 04_NARROWING/index.js
@@ -14,3 +14,22 @@ function sum(a, b) {
 sum(1, 2);
 sum("4", "4");
 sum("5", 1);
+// 2 - checando se valor existe
+function operations(arr, operation) {
+    if (operation) {
+        if (operation === "sum") {
+            const sum = arr.reduce((i, total) => i + total);
+            console.log(sum);
+        }
+        else if (operation === "multiply") {
+            const multiply = arr.reduce((i, total) => i * total);
+            console.log(multiply);
+        }
+    }
+    else {
+        console.log("Por favor, defina uma operação!");
+    }
+}
+operations([10, 20, 30], "sum");
+operations([10, 20, 30], "multiply");
+operations([10, 20, 30], "");
--- 04_NARROWING/index.ts
+++ 04_NARROWING/index.ts
@@ -12,3 +12,22 @@ function sum(a: number | string, b: number | string) {
 sum(1, 2);
 sum("4", "4");
 sum("5", 1);
+
+// 2 - checando se valor existe
+function operations(arr: number[], operation?: string | undefined) {
+  if (operation) {
+    if (operation === "sum") {
+      const sum = arr.reduce((i, total) => i + total);
+      console.log(sum);
+    } else if (operation === "multiply") {
+      const multiply = arr.reduce((i, total) => i * total);
+      console.log(multiply);
+    }
+  } else {
+    console.log("Por favor, defina uma operação!");
+  }
+}
+
+operations([10, 20, 30], "sum");
+operations([10, 20, 30], "multiply");
+operations([10, 20, 30], "");
