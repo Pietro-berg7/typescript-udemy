b82d8595389d19f32e3f7f0fc98e12f66a950cb5
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-03-30 23:43:39 +0044

    Exemplo real de method decorator

--- 10_DECORATORS/index.js
+++ 10_DECORATORS/index.js
@@ -163,3 +163,35 @@ const pen = new Pen(55);
 console.log(newBook);
 console.log(pen);
 console.log(newBook.createdAt);
+// 8 - exemplo real method decorator
+function checkIfUserPosted() {
+    return function (target, key, descriptor) {
+        const childFunction = descriptor.value;
+        console.log(childFunction);
+        descriptor.value = function (...args) {
+            if (args[1] === true) {
+                console.log("Usuário já postou!");
+                return null;
+            }
+            else {
+                return childFunction.apply(this, args);
+            }
+        };
+        return descriptor;
+    };
+}
+class Post {
+    constructor() {
+        this.alreadyPosted = false;
+    }
+    post(content, alreadyPosted) {
+        this.alreadyPosted = true;
+        console.log(`Post do usuário: ${content}`);
+    }
+}
+__decorate([
+    checkIfUserPosted()
+], Post.prototype, "post", null);
+const newPost = new Post();
+newPost.post("Meu primeiro post!", newPost.alreadyPosted);
+newPost.post("Meu segundo post!", newPost.alreadyPosted);
--- 10_DECORATORS/index.ts
+++ 10_DECORATORS/index.ts
@@ -211,3 +211,43 @@ console.log(newBook);
 console.log(pen);
 
 console.log(newBook.createdAt);
+
+// 8 - exemplo real method decorator
+function checkIfUserPosted() {
+  return function (
+    target: Object,
+    key: string | Symbol,
+    descriptor: PropertyDescriptor
+  ) {
+    const childFunction = descriptor.value;
+
+    console.log(childFunction);
+
+    descriptor.value = function (...args: any[]) {
+      if (args[1] === true) {
+        console.log("Usuário já postou!");
+        return null;
+      } else {
+        return childFunction.apply(this, args);
+      }
+    };
+
+    return descriptor;
+  };
+}
+
+class Post {
+  alreadyPosted = false;
+
+  @checkIfUserPosted()
+  post(content: string, alreadyPosted: boolean) {
+    this.alreadyPosted = true;
+    console.log(`Post do usuário: ${content}`);
+  }
+}
+
+const newPost = new Post();
+
+newPost.post("Meu primeiro post!", newPost.alreadyPosted);
+
+newPost.post("Meu segundo post!", newPost.alreadyPosted);
