a4ed8f82fefcece5e80e68cf1a6d1c9b706c4eaa
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-03-31 19:17:19 +0044

    Desestruturando props

--- 11_REACT_COM_TS/src/App.tsx
+++ 11_REACT_COM_TS/src/App.tsx
@@ -3,6 +3,10 @@ import React from "react";
 // 4 - Importação de componentes
 import FirstComponent from "./components/FirstComponent";
 
+// 5 - Desestruturando props
+import SecondComponent from "./components/SecondComponent";
+import Destructuring from "./components/Destructuring";
+
 function App() {
   // 1 - Variáveis
   const name: string = "Pietro";
@@ -26,6 +30,13 @@ function App() {
       )}
       <h3>{userGreeting(name)}</h3>
       <FirstComponent />
+      <SecondComponent name="Segundo" />
+      <Destructuring
+        title="Primeiro post"
+        content="Algum conteúdo"
+        commentsQty={10}
+        tags={["ts", "js"]}
+      />
     </div>
   );
 }
