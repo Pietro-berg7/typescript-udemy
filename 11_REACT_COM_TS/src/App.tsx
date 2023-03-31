e12409678f4630a97d7891c58d687f2bfa95cc8c
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-03-31 05:09:41 +0044

    Variáveis e funções em componentes

--- 11_REACT_COM_TS/src/App.tsx
+++ 11_REACT_COM_TS/src/App.tsx
@@ -1,7 +1,27 @@
+import React from "react";
+
 function App() {
+  // 1 - Variáveis
+  const name: string = "Pietro";
+  const age: number = 22;
+  const isWorking: boolean = false;
+
+  // 2 - Funções
+  const userGreeting = (name: string): string => {
+    return `Olá, ${name}!`;
+  };
+
   return (
     <div className="App">
       <h1>TypeScript com React</h1>
+      <h2>Nome: {name}</h2>
+      <p>Idade: {age}</p>
+      {isWorking && (
+        <div>
+          <p>Está trabalhando!</p>
+        </div>
+      )}
+      <h3>{userGreeting(name)}</h3>
     </div>
   );
 }
