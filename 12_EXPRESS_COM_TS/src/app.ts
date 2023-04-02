64e05cc456a0696dcb7d03a0d11551d72c1bc3b4
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-02 01:17:56 +0044

    rota com post & rota all

--- 12_EXPRESS_COM_TS/src/app.ts
+++ 12_EXPRESS_COM_TS/src/app.ts
@@ -2,12 +2,35 @@
 // console.log("Hello Express + TS");
 
 // 2 - init express
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 
 const app = express();
 
+// 3 - rota com POST
+app.use(express.json());
+
 app.get("/", (req, res) => {
   return res.send("Hello Express!");
+});
+
+// 3 - rota com POST
+app.post("/api/product", (req, res) => {
+  console.log(req.body);
+
+  return res.send("Produto adicionado!");
+});
+
+// 4 - rota para todos os verbos
+app.all("/api/product/check", (req, res) => {
+  // req.method = VERBO HTTP
+
+  if (req.method === "POST") {
+    return res.send("Inseriu algum registro!");
+  } else if (req.method === "GET") {
+    return res.send("Leu algum registro!");
+  } else {
+    return res.send("Não podemos realizar esta operação!");
+  }
 });
 
 app.listen(3000, () => {
